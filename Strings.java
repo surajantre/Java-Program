84a27ee54d68a3eef71c4d99d3f7e5c1f89ec78b
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-04-10 17:27:30 +0530

    ,,

--- Strings.java
+++ Strings.java
@@ -4,9 +4,9 @@
 //         for(int i=0; i<path.length(); i++){
 //             char dir = path.charAt(i);
 //             // south
-/            if(dir =='s'){
+//            if(dir =='s'){
 //                 y--;
-//             }
+/             }
 //             //north
 //             else if(dir == 'n'){
 //                 y++;
